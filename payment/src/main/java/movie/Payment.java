df62b42b5eb6ee6b0a96219e14d16a3564399b38
clialsh <clialsh@gmail.com>
2021-02-23 13:34:25 +0900

    서킷 브레이킹 로직 추가

--- payment/src/main/java/movie/Payment.java
+++ payment/src/main/java/movie/Payment.java
@@ -30,6 +30,12 @@ public class Payment {
 
         }
 
+        try {
+            Thread.currentThread().sleep((long) (400 + Math.random() * 220));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public Long getId() {
